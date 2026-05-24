0cc8db8135ca78743c9e9b45b935394464a55686
lourirsn <rodrigueslouri010@gmail.com>
2026-05-24 16:36:32 -0300

    banco

--- banco.py
+++ banco.py
@@ -1,2 +1,31 @@
 import sqlite3
 
+#conectar
+conexao = sqlite3.connect("esports.db")
+
+cursor = conexao.cursor()
+
+#tabela de jogadores 
+cursor.execute(""" 
+    create table if not exists jogadores(
+        id integer primary key autoincrement,
+        nome text,
+        nickname text,
+        jogo text,
+        nivel text)
+    """)
+
+#tabela  de treinos
+cursor.execute(""" 
+    create table if not exists treinos(
+        id integer primary key autoincrement,
+        jogador text,
+        horas text,
+        data text,
+        observacao text)
+    """)
+
+conexao.commit()
+conexao.close()
+
+print("banco criado com sucesso")
